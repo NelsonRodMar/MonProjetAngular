96462e34791462f8cf2e65c9b6d99c77f46b8150
Nelson Rodrigues Marreiros <nrodrigues@pcm.eu>
2019-07-24 17:15:32 +0200

    feat : add routes
    add some routes following the begin of the chapter 'Gérez la navigation avec le Routing'
    
    - Nelson Rodrigues Marreiros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
@@ -7,38 +6,7 @@ import {AppareilService} from './services/appareil.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
 
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Frigo';
-  appareilThree = 'Ordinateur';
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-  onAllumer() {
-    this.appareilService.swtchOnAll();
-  }
-  onEteindre() {
-    if (confirm('Etes-vous sûre de vouloir éteindre tous vos appareils ?')){
-      this.appareilService.swtchOffAll();
-    } else {
-      return null;
-    }}
+  constructor() {}
+  ngOnInit() {}
 }
